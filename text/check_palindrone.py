fd042f06c3c6ecfb04ad023f3b36921bdfcdbbe7
Michael Hixon <michael.hixon@hotmail.com>
2017-09-01 17:32:30 -0600

    added gitignore and palindrone/count_words impl's

---
+++ text/check_palindrone.py
@@ -0,0 +1,17 @@
+"""Palindrome checker"""
+
+import str_reverse
+
+def check_palindrome(string):
+    """Returns true if string has palindrome, false otherwise."""
+    if string == str_reverse.str_reverse(string):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    import sys
+    if check_palindrome(sys.argv[1]):
+        print "True"
+    else:
+        print "False"
---
+++ text/count_words.py
@@ -0,0 +1,12 @@
+"""Counts words"""
+
+def count_words(string):
+    """Returns the # of words in a particular string"""
+    words = string.split(' ')
+    filtered_words = [x for x in words if x != '']
+    return len(filtered_words)
+
+
+if __name__ == "__main__":
+    import sys
+    print count_words(sys.argv[1])
