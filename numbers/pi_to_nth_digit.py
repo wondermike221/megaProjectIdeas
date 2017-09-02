2848f097f74a91c5544387d27d02cb0ab906d432
Michael Hixon <michael.hixon@hotmail.com>
2017-09-01 19:44:36 -0600

    fixed pylint indentation errors

--- numbers/pi_to_nth_digit.py
+++ numbers/pi_to_nth_digit.py
@@ -10,12 +10,12 @@ def pi_to_nth_digit(digits):
     digits = int(digits)
     if digits > 100:
         digits = 100
-    getcontext().prec=digits
+    getcontext().prec = digits
     pi_to_n = sum(1/Decimal(16)**k *
-        (Decimal(4)/(8*k+1) -
-        Decimal(2)/(8*k+4) -
-        Decimal(1)/(8*k+5) -
-        Decimal(1)/(8*k+6)) for k in range(100))
+                  (Decimal(4)/(8*k+1) -
+                   Decimal(2)/(8*k+4) -
+                   Decimal(1)/(8*k+5) -
+                   Decimal(1)/(8*k+6)) for k in range(100))
     return pi_to_n
 
 
