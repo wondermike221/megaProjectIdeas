4ef04801f2d9895bd86ac912c10492e5da57ee10
Michael Hixon <michael.hixon@hotmail.com>
2017-09-01 19:01:05 -0600

    done

--- text/vowel_count.py
+++ text/vowel_count.py
@@ -1,14 +1,14 @@
 """Vowel Count"""
 
 def vowel_count(string):
-    #TODO
-    return string
-
-def _output_vowel_stats(string):
-    vowel_stats = vowel_count(string)
-    print vowel_stats
-    #TODO
+    """Returns a dict where each vowel in the alphabet exists as a key and contains the count of the vowels"""
+    vowels = ('a', 'e', 'i', 'o', 'u', 'y')
+    vowels_count = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0, 'y':0}
+    for x in string:
+        if x in vowels:
+            vowels_count[x] += 1
+    return vowels_count
 
 if __name__ == "__main__":
     import sys
-    _output_vowel_stats(sys.argv[1])
+    print(vowel_count(sys.argv[1]))
