ce4d56626f67800650f7fc2d663b531c15543245
Michael Hixon <michael.hixon@hotmail.com>
2017-09-06 14:45:33 -0600

    aliased python to python3, fixing some problems with python 2 to 3

--- numbers/prime_factorization.py
+++ numbers/prime_factorization.py
@@ -3,15 +3,15 @@ import math
 
 def prime_factors(number):
     factors = []
-    while number & 1 == 0:
+    while int(number) & int(1) == 0:
         factors.append(2)
         factors.append(number)
-        number = number/2
+        number = int(number/2)
     for i in range(3, int(math.sqrt(number))+1, 2):
-        while number % i == 0:
-            factors.append(i)
+        while int(number) % int(i) == 0:
+            factors.append(int(i))
             factors.append(number)
-            number = number/i
+            number = int(number/i)
     if number > 2:
         factors.append(number)
     factors.sort()
